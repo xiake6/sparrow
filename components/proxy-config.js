d4273c8d6497cbc6a6eab9dcc0d080a2b44f497c
jadestar <yuxing.zuo@ly.com>
2019-11-21 16:29:36 +0800

    配置文件路径调整

--- components/proxy-config.js
+++ components/proxy-config.js
@@ -1,4 +1,3 @@
-const utils = require("../engineering/utils.js");
 const environmentConfig = require("../config/environmentConfig.js");
 // console.log("process.env.NODE_ENV:",process.env.NODE_ENV);
 
@@ -6,7 +5,7 @@ const environmentConfig = require("../config/environmentConfig.js");
 module.exports = {
     //代理请求到线上环境
     "/xiake/components": {
-        target: environmentConfig["www.xiake666.com"][utils.envconfig],
+        target: environmentConfig["www.xiake666.com"],
         changeOrigin: true,
     },
 };
